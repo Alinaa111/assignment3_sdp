a23a6aa7167b9b8ff5d1fc1683ede8060277beca
alina <alinamoldabaeva1704@gmail.com>
2025-09-28 14:05:23 +0500

    Added interface MetricConverter

---
+++ src/MetricConverter.java
@@ -0,0 +1,6 @@
+// Target interface with abstract methods
+public interface MetricConverter {
+    double getKilometers(double miles);
+    double getCelsius(double fahrenheit);
+    double getLiters(double gallons);
+}
